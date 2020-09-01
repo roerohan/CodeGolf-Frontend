0e656c5092140ce26ed3805eef278debcc820fb5
Subham Panda <im.sspanda10@gmail.com>
2020-09-02 04:14:04 +0530

    feat: basic folder structure, eslint setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+
+const App = () => (
+    <div className="App">
+        <h1>CODEGOLF</h1>
+    </div>
+);
+
+export default App;
