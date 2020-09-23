fbce02003f7ae7b3d780c3c08c7e6597a4d363cb
ashikka <ashikagupta28@gmail.com>
2020-09-24 03:03:59 +0530

    feat: Added HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import header from "../../assets/HomePage/YOUGOTAsset 1.png";
+import '../../assets/HACKED/HACKED.ttf';
 
-import './HomePage.css';
+import "./HomePage.css";
 
 const HomePage = () => {
-	return (
-		<h1>HOMEPAGE</h1>
-	)
-}
+  return (
+    <div className="container">
+      <img className="header" src={header} alt="header.png" />
+      <div className="title">
+        <h1>C G</h1>
+		    <h1 className="heading">Codegolf</h1>
+        <div className="button">Press any key to continue</div>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
