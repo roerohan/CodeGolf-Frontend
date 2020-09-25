c2d1abd1296f79fa516aafcc36b47d3a7ff88a5c
ashikka <ashikagupta28@gmail.com>
2020-09-26 03:57:38 +0530

    fix: Linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-indent */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
@@ -6,12 +5,12 @@ import HomePage from './pages/HomePage/HomePage';
 import QuestionsPage from './pages/QuestionsPage/QuestionsPage';
 
 const App = () => (
-    <div className="App">
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/main" component={QuestionsPage} />
-        </Switch>
-    </div>
+  <div className="App">
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/main" component={QuestionsPage} />
+    </Switch>
+  </div>
 );
 
 export default App;
