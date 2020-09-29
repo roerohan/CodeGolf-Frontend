98e374123c235d8b97692a937ece2abb329366bc
ashikka <ashikagupta28@gmail.com>
2020-09-30 04:54:53 +0530

    fix: Modified modal

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,8 +1,8 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 
 import ModalButton from "../../assets/Modal/Modal-Button.svg";
-import ModalBody from "../../assets/Modal/Modal.png";
+// import ModalBody from "../../assets/Modal/Modal.png";
 import "./modal.css";
 
 const ModalBox = () => {
@@ -13,10 +13,30 @@ const ModalBox = () => {
 
   return (
     <>
-      <img src={ModalButton} alt="modal-button.svg" className= "modal-button" onClick={handleShow} />
+      <img
+        src={ModalButton}
+        alt="modal-button.svg"
+        className="modal-button"
+        onClick={handleShow}
+      />
 
-      <Modal show={show} onHide={handleClose}>
-          <img src={ModalBody} alt="modal.png"/>
+      <Modal className="modal" show={show} onHide={handleClose}>
+        <Modal.Body className="modal-body">
+          <div className="modal-image">
+          </div>
+          <h1>About</h1>
+          <div className="modal-content">
+            <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            </h3>
+          </div>
+        </Modal.Body>
       </Modal>
     </>
   );
